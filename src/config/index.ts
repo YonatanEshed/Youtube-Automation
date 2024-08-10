96b1d6b5ea1def08956e388d25d862dee4b4c3bb
Yonatan Eshed <yonatan.eshed@gmail.com>
2024-08-10 18:08:57 +0300

    feat(config): add jwtSecret to configuration

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,11 +5,14 @@ config();
 interface Config {
     port: number;
     nodeEnv: string;
+    jwtSecret: string;
 }
 
 const configuration: Config = {
-    port: parseInt(process.env.PORT || '3000', 10),
+    port: parseInt(process.env.PORT || '3000'),
     nodeEnv: process.env.NODE_ENV || 'development',
+
+    jwtSecret: process.env.JWT_SECRET || '',
 };
 
 export default configuration;

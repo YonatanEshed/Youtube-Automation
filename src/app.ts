7bb664b10f9cfcc56e11763d5514418770dd2062
Yonatan Eshed <yonatan.eshed@gmail.com>
2024-08-10 18:20:15 +0300

    feat: connect authRouter to application

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,16 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+
 import configuration from './config';
+import authRouter from './routes/auth.routes';
 
 const app = express();
 const port = configuration.port;
+
+app.use(bodyParser.json({ limit: '100mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
+app.use('/api/auth', authRouter);
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
